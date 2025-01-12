2415ae52108b8f941929e34b1b3bba7a0d8c71b0
JustinFreitas <justin.s.freitas@me.com>
2025-01-12 14:33:56 -0700

    filter out item-piles actors

--- found-items-all-actors.js
+++ found-items-all-actors.js
@@ -1,11 +1,12 @@
 const allFoundItems = [];
-game.actors.forEach(actor => {
-    const foundItems = actor.items.filter(item => item.type === 'item' && item.name.includes('(Found)'));
-    foundItems.forEach(item => {
-        const found = `<b>${actor.name}:</b> ${item.name}`;
-        console.log(found);
-        allFoundItems.push(found);
-    });
+game.actors.filter(actor => !actor.flags['item-piles']?.data?.enabled)
+           .forEach(actor => {
+                const foundItems = actor.items.filter(item => item.type === 'item' && item.name.includes('(Found)'));
+                foundItems.forEach(item => {
+                    const found = `<b>${actor.name}:</b> ${item.name}`;
+                    console.log(found);
+                    allFoundItems.push(found);
+           });
 });
 
 if (allFoundItems.length > 0) {
