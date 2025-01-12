24c4fe08ad5edb1e46299d9f138d35fc4d2387c4
JustinFreitas <justin.s.freitas@me.com>
2025-01-12 14:50:34 -0700

    check and remove phantom items from brian

---
+++ remove-phantom-items.js
@@ -0,0 +1,16 @@
+/* Macro to fix items that may be stuck hidden because
+ * they have a containerId value but the container can't be found
+ * or two containers that contain each other */
+
+// runs on all actors, checks only... does not remove.
+game.actors.forEach(actor => {
+    const buggedItems = actor.items.filter(item => {
+        return item.system.containerId !== '' &&
+        item.type === 'item' &&
+        actor.items.get(item.system.containerId) === undefined;
+    });
+    buggedItems.forEach(item => {
+        console.log(`${actor.name}:`, item.name);
+        item.delete();
+    });
+});
