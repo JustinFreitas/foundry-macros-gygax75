060861742b7087e9e34e5c7bf11d158ed98d9e18
JustinFreitas <justin.s.freitas@me.com>
2025-01-12 17:55:10 -0700

    dungeon speed initial implementation.

---
+++ slowest-dungeon-speed.js
@@ -0,0 +1,12 @@
+const partyActors = game.actors.filter(actor => actor.flags.ose?.party === true);
+const speedToActorMap = new Map();
+partyActors.forEach(actor => {
+    const actorSpeed = actor.system.movement.base;
+    const charsWithSpeed = speedToActorMap.get(actorSpeed) || [];
+    speedToActorMap.set(actorSpeed, [actor, ...charsWithSpeed].sort((a, b) => ('' + a.name).localeCompare('' + b.name)));
+});
+
+const slowestSpeed = Math.min(...speedToActorMap.keys());
+const actorsWithSlowestSpeed = speedToActorMap.get(slowestSpeed);
+const output = `The party's slowest speed is ${slowestSpeed}/${slowestSpeed/3}.  Characters: ${actorsWithSlowestSpeed.map(a => a.name).join(", ")}`;
+ChatMessage.create({content: '<h2>Slowest Dungeon Speed</h2>' + output});
