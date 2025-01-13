5ffce24effec1fd1658b3e7346bac08f412bd8b8
JustinFreitas <justin.s.freitas@me.com>
2025-01-12 18:50:29 -0700

    brian formatting

--- slowest-dungeon-speed.js
+++ slowest-dungeon-speed.js
@@ -8,5 +8,5 @@ partyActors.forEach(actor => {
 
 const slowestSpeed = Math.min(...speedToActorMap.keys());
 const actorsWithSlowestSpeed = speedToActorMap.get(slowestSpeed);
-const output = `The party's slowest speed is ${slowestSpeed}.<br/><br/>Characters: ${actorsWithSlowestSpeed.map(a => a.name).join(", ")}`;
-ChatMessage.create({content: '<h2>Slowest Dungeon Speed</h2>' + output});
+const output = `The party's slowest speed is <b>${slowestSpeed}ft</b> per turn.<br/><br/>Characters: ${actorsWithSlowestSpeed.map(a => a.name).join(", ")}`;
+ChatMessage.create({content: '<h2>Dungeon Speed Report</h2>' + output});
