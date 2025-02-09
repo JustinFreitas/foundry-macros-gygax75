4e69acb02d3d388b30e1c7f05cbc5d82e89a7a45
JustinFreitas <justin.s.freitas@me.com>
2025-02-09 15:22:42 -0700

    add item count to output

--- found-items-all-actors.js
+++ found-items-all-actors.js
@@ -3,7 +3,7 @@ game.actors.filter(actor => !actor.flags['item-piles']?.data?.enabled)
            .forEach(actor => {
                 const foundItems = actor.items.filter(item => item.type === 'item' && item.name.includes('(Found)'));
                 foundItems.forEach(item => {
-                    const found = `<b>${actor.name}:</b> ${item.name}`;
+                    const found = `<b>${actor.name}:</b> ${item.system.quantity.value || 0} ${item.name}`;
                     console.log(found);
                     allFoundItems.push(found);
            });
