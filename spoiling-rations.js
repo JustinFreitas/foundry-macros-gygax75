c37b955aa57ae46e80c31398aaf90b30c2d4aeb3
JustinFreitas <justin.s.freitas@me.com>
2025-02-09 14:53:22 -0700

    initial implementation

---
+++ spoiling-rations.js
@@ -0,0 +1,47 @@
+const DAY = 8.64e+7;
+const partyActors = game.actors.filter(actor => actor.flags.ose?.party === true);
+const nameToRationsMap = new Map();
+let ironExpirationDateString;
+let standardExpirationDateString;
+if (typeof SimpleCalendar !== 'undefined') {
+    const currentTimestamp = SimpleCalendar.api.timestamp();
+    standardExpirationDateString = SimpleCalendar.api.formatTimestamp(currentTimestamp, 'M/D/YYYY');
+    const expirationTimestamp = SimpleCalendar.api.timestampPlusInterval(currentTimestamp, {day: 7});
+    ironExpirationDateString = SimpleCalendar.api.formatTimestamp(expirationTimestamp, 'M/D/YYYY');
+} else {
+    const currentDate = new Date(Date.now());
+    standardExpirationDateString = currentDate.toLocaleDateString();
+    const expirationDateValue = DAY * 7;
+    const expirationDate = new Date(Date.now() + expirationDateValue);
+    ironExpirationDateString = expirationDate.toLocaleDateString();
+}
+
+partyActors.forEach(actor => {
+    actor.items.forEach(item => {
+        const foundStandard = item.name.match(/rations, standard \((?<date>[^)]+)\)/i);
+        const foundIron = item.name.match(/rations, iron \((?<date>[^)]+)\)/i);
+        let newName;
+        if (foundStandard?.groups?.date && new Date(foundStandard.groups.date) > new Date(standardExpirationDateString)) {
+            newName = `Rations, standard (${standardExpirationDateString})`;
+        } else if (foundIron?.groups?.date && new Date(foundIron.groups.date) > new Date(ironExpirationDateString)) {
+            newName = `Rations, iron (${ironExpirationDateString})`;
+        }
+
+        if (newName) {
+            item.update({name: newName});
+            const rationUpdate = `${item.name} changed to ${newName}`;
+            nameToRationsMap.set(actor.name, [...nameToRationsMap.get(actor.name) ? nameToRationsMap.get(actor.name) : [], rationUpdate]);        
+        }
+    });
+});
+
+if (nameToRationsMap.keys().toArray().length > 0) {
+    const collatedRations = [];
+    nameToRationsMap.keys().forEach(actorName => {
+        collatedRations.push(`<b>${actorName}:</b>  ${nameToRationsMap.get(actorName).sort().join(", ")}<br/>`);
+    });
+
+    ChatMessage.create({content: '<h2>Rations Spoiling Report</h2>' + collatedRations.join('<br/>')});
+} else {
+    ChatMessage.create({content: '<h2>Rations Spoiling Report</h2><br/>No rations found in the party that need additional spoiling.'});
+}
