2b9dedd0bb4f4bee871ee06385647339f4b4de52
JustinFreitas <justin.s.freitas@me.com>
2025-02-09 15:30:54 -0700

    collate the items by character

--- found-items-all-actors.js
+++ found-items-all-actors.js
@@ -1,16 +1,20 @@
-const allFoundItems = [];
+const nameToItemsMap = new Map();
 game.actors.filter(actor => !actor.flags['item-piles']?.data?.enabled)
            .forEach(actor => {
                 const foundItems = actor.items.filter(item => item.type === 'item' && item.name.includes('(Found)'));
                 foundItems.forEach(item => {
-                    const found = `<b>${actor.name}:</b> ${item.system.quantity.value || 0} ${item.name}`;
-                    console.log(found);
-                    allFoundItems.push(found);
-           });
-});
+                    const found = `${item.system.quantity.value || 0} ${item.name}`;
+                    nameToItemsMap.set(actor.name, [...nameToItemsMap.get(actor.name) ? nameToItemsMap.get(actor.name) : [], found]);
+                });
+            });
 
-if (allFoundItems.length > 0) {
-    ChatMessage.create({content: '<h2>Found Treasure Report</h2><br/>' + allFoundItems.join('<br/><br/>')});
+if (nameToItemsMap.keys().toArray().length > 0) {
+    const collatedItems = [];
+    nameToItemsMap.keys().forEach(actorName => {
+        collatedItems.push(`<b>${actorName}:</b>  ${nameToItemsMap.get(actorName).sort().join(", ")}<br/>`);
+    });
+
+    ChatMessage.create({content: '<h2>Found Treasure Report</h2>' + collatedItems.join('<br/>')});
 } else {
     ChatMessage.create({content: '<h2>Found Treasure Report</h2><br/>No (Found) items in any actor.'});
 }
