5e1b0c979fa2751f85c90bc084f0f0d51a8dca50
JustinFreitas <justin.s.freitas@me.com>
2025-02-08 17:25:53 -0700

    SimpleCalendar support

--- expired-ration-removal.js
+++ expired-ration-removal.js
@@ -2,12 +2,16 @@ const violations = [];
 const partySheetActors = game.actors.filter(actor => actor.flags.ose?.party === true)
 for (let i = 0; i < partySheetActors.length; i++) {
     const actor = partySheetActors[i];
-    console.log(`Actor ${i}`, actor);
     actor.items.forEach(item => {
         const found = item.name.match(/rations, (iron|standard) \((?<date>[^)]+)\)/i);
         if (found?.groups?.date) {
             const rationDate = new Date(found.groups.date);
-            const todayDate = new Date(new Date().toDateString());
+            let todayDate = new Date(new Date().toDateString());
+            if (typeof SimpleCalendar !== 'undefined') {
+                const currentTimestamp = SimpleCalendar.api.timestamp();
+                todayDate = new Date(SimpleCalendar.api.formatTimestamp(currentTimestamp, 'M/D/YYYY'));
+            }
+
             if (rationDate < todayDate) {
                 const violation = `<b>${actor.name}:</b> ${item.name} was removed.`;
                 console.log(violation);
