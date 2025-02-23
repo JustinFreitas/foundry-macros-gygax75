fcd0fd7d9269b8df81889d6556a35aee6a2306c8
JustinFreitas <justin.s.freitas@me.com>
2025-02-23 08:42:32 -0700

    Toggle Bless initial implementation

---
+++ toggle-bless-selected-tokens.js
@@ -0,0 +1,30 @@
+const BLESS_EFFECT_NAME = 'bless';
+const tokens = canvas.tokens.controlled;
+const nameToBonusAndWeaponsTupleMap = new Map();
+tokens.forEach(token => {
+    const actor = token.actor;
+    let bonus = actor.statuses.has(BLESS_EFFECT_NAME) ? -1 : 1;
+    actor.toggleStatusEffect(BLESS_EFFECT_NAME);
+    const weapons = [];
+    // Alter bonus on weapons.
+    actor.items.filter(i=>i.type==='weapon').forEach(item => {
+        item.update({system: {bonus: item.system.bonus + bonus}});
+        weapons.push(item.name);
+    });
+
+    nameToBonusAndWeaponsTupleMap.set(actor.name, { bonus: bonus, weapons: weapons });
+});
+
+if (nameToBonusAndWeaponsTupleMap.keys().toArray().length > 0) {
+    const collatedItems = [];
+    nameToBonusAndWeaponsTupleMap.keys().forEach(actorName => {
+        const bonusAndWeaponsTuple = nameToBonusAndWeaponsTupleMap.get(actorName);
+        const weaponsOutput = bonusAndWeaponsTuple.weapons.length > 0 ? bonusAndWeaponsTuple.weapons.sort().join(", ") : 'No weapons modified';
+        const addingOrRemoving = bonusAndWeaponsTuple.bonus > 0 ? 'Adding' : 'Removing';
+        collatedItems.push(`<b>${actorName}:</b>  ${addingOrRemoving} Bless.  ${weaponsOutput}.<br/>`);
+    });
+
+    ChatMessage.create({content: '<h2>Toggle Bless Report</h2>' + collatedItems.join('<br/>')});
+} else {
+    ChatMessage.create({content: '<h2>Toggle Bless Report</h2>No tokens selected.'});
+}
