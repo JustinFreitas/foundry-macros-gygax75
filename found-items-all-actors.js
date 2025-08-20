f7b71434f40f311de282d15ce13e6e39f53eb160
JustinFreitas <justin.s.freitas@me.com>
2025-08-19 19:26:32 -0600

    Some refactoring to use functionless looping

--- found-items-all-actors.js
+++ found-items-all-actors.js
@@ -1,20 +1,34 @@
 const nameToItemsMap = new Map();
-game.actors.filter(actor => !actor.flags['item-piles']?.data?.enabled)
-           .forEach(actor => {
-                const foundItems = actor.items.filter(item => item.type === 'item' && item.name.includes('(Found)'));
-                foundItems.forEach(item => {
-                    const found = `${item.system.quantity.value || 0} ${item.name}`;
-                    nameToItemsMap.set(actor.name, [...nameToItemsMap.get(actor.name) ? nameToItemsMap.get(actor.name) : [], found]);
-                });
-            });
+const nonItemPilesActors = game.actors.filter(
+    (actor) => !actor.flags["item-piles"]?.data?.enabled
+);
+for (const actor of nonItemPilesActors) {
+    const foundItems = actor.items.filter(
+        (item) => item.type === "item" && item.name?.includes("(Found)")
+    );
+    for (const item of foundItems) {
+        const found = `${item.system.quantity.value || 0} ${item.name}`;
+        nameToItemsMap.set(actor.name, [
+            ...(nameToItemsMap.get(actor.name) ?? []),
+            found,
+        ]);
+    }
+}
 
 if (nameToItemsMap.keys().toArray().length > 0) {
     const collatedItems = [];
-    nameToItemsMap.keys().forEach(actorName => {
-        collatedItems.push(`<b>${actorName}:</b>  ${nameToItemsMap.get(actorName).sort().join(", ")}<br/>`);
-    });
+    for (const actorName of nameToItemsMap.keys()) {
+        collatedItems.push(
+            `<b>${actorName}:</b>  ${nameToItemsMap.get(actorName).sort().join(", ")}<br/>`
+        );
+    }
 
-    ChatMessage.create({content: '<h2>Found Treasure Report</h2>' + collatedItems.join('<br/>')});
+    ChatMessage.create({
+        content: "<h2>Found Treasure Report</h2>" + collatedItems.join("<br/>"),
+    });
 } else {
-    ChatMessage.create({content: '<h2>Found Treasure Report</h2><br/>No (Found) items in any actor.'});
+    ChatMessage.create({
+        content:
+            "<h2>Found Treasure Report</h2><br/>No (Found) items in any actor.",
+    });
 }
