16f299008cae631f3a3aeaa84f702b45362c33d1
JustinFreitas <justin.s.freitas@me.com>
2025-09-01 21:04:26 -0600

    Better looping and show quantity of removed item.

---
+++ expired-ration-removal-all.js
@@ -0,0 +1,27 @@
+const violations = [];
+for (const actor of game.actors) {
+    for (const item of actor.items) {
+        const found = item.name.match(/rations, (iron|standard|fresh food|preserved meat) \((?<date>[^)]+)\)/i);
+        if (found?.groups?.date) {
+            const rationDate = new Date(found.groups.date);
+            let todayDate = new Date(new Date().toDateString());
+            if (typeof SimpleCalendar !== 'undefined') {
+                const currentTimestamp = SimpleCalendar.api.timestamp();
+                todayDate = new Date(SimpleCalendar.api.formatTimestamp(currentTimestamp, 'M/D/YYYY'));
+            }
+
+            if (rationDate < todayDate) {
+                const violation = `<b>${actor.name}:</b> ${item.name} with quantity ${item.system.quantity.value} was removed.`;
+                console.log(violation);
+                violations.push(violation);
+                item.delete();
+            }
+        }
+    };
+}
+
+if (violations.length > 0) {
+    ChatMessage.create({content: '<h4>Expired Rations Report - All</h4>' + violations.join('<br/><br/>')});
+} else {
+    ChatMessage.create({content: '<h4>Expired Rations Report - All</h4>No rations were deleted.'});
+}
--- expired-ration-removal-party.js
+++ expired-ration-removal-party.js
@@ -1,8 +1,7 @@
 const violations = [];
 const partySheetActors = game.actors.filter(actor => actor.flags.ose?.party === true);
-for (let i = 0; i < partySheetActors.length; i++) {
-    const actor = partySheetActors[i];
-    actor.items.forEach(item => {
+for (const actor of partySheetActors) {
+    for (const item of actor.items) {
         const found = item.name.match(/rations, (iron|standard|fresh food|preserved meat) \((?<date>[^)]+)\)/i);
         if (found?.groups?.date) {
             const rationDate = new Date(found.groups.date);
@@ -13,17 +12,17 @@ for (let i = 0; i < partySheetActors.length; i++) {
             }
 
             if (rationDate < todayDate) {
-                const violation = `<b>${actor.name}:</b> ${item.name} was removed.`;
+                const violation = `<b>${actor.name}:</b> ${item.name} with quantity ${item.system.quantity.value} was removed.`;
                 console.log(violation);
                 violations.push(violation);
                 item.delete();
             }
         }
-    });
+    }
 }
 
 if (violations.length > 0) {
-    ChatMessage.create({content: '<h2>Expired Rations Report</h2><br/>' + violations.join('<br/><br/>')});
+    ChatMessage.create({content: '<h4>Expired Rations Report - Party</h4>' + violations.join('<br/><br/>')});
 } else {
-    ChatMessage.create({content: '<h2>Expired Rations Report</h2><br/>No rations were deleted.'});
+    ChatMessage.create({content: '<h4>Expired Rations Report - Party</h4>No rations were deleted.'});
 }
