49c178deb8dee309ecd179e130b66c52680af3d0
JustinFreitas <justin.s.freitas@me.com>
2025-09-12 17:48:36 -0600

    Improved chat reporting.

--- close-all-doors-active-scene.js
+++ close-all-doors-active-scene.js
@@ -1,1 +1,13 @@
-game.scenes.active.walls.filter(w=>!!w.door && !!w.ds).forEach(w=>w.update({ds:0}));
+const openDoors = game.scenes.active.walls.filter(w=>!!w.door && !!w.ds);
+let content;
+if (openDoors.length === 0) {
+    content = '<h4>No Open Doors Found</h4>';
+} else {
+    content = '<h4>All Doors Closed</h4>';
+    openDoors.forEach(w=>w.update({ds:0}));
+}
+
+await ChatMessage.create({
+    content: content,
+    whisper: [game.userId]
+});
