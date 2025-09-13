d0d23fa76c2f37ba611d5434ff814facc90e3470
JustinFreitas <justin.s.freitas@me.com>
2025-09-13 10:51:43 -0600

    macro to close all windows

--- close-all-windows.js
+++ close-all-windows.js
@@ -1,1 +1,11 @@
+// Close all journal/actor/item sheets.
 Object.values(ui.windows).forEach(w => w.close());
+
+// Close all tables.
+ui.tables.collection.forEach(t=>t.sheet.close());
+
+// Close all macros.
+ui.macros.collection.forEach(m=>m.sheet.close());
+
+// Close all journals.
+ui.journal.collection.forEach(j=>j.sheet.close());
